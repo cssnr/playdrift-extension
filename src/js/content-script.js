57455c7cf9b75dda946cf942c1626b2dd997b4f4
Shane <6071159+smashedr@users.noreply.github.com>
2024-03-28 15:26:59 -0700

    Update content-script.js listener and functions

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -9,74 +9,77 @@
 //     // console.log('response:', response)
 // })()
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.debug('message:', message)
+chrome.runtime.onMessage.addListener(onMessage)
+
+/**
+ * On Message Callback
+ * @function onMessage
+ * @param {Object} message
+ * @param {MessageSender} sender
+ * @param {Function} sendResponse
+ */
+async function onMessage(message, sender, sendResponse) {
+    console.log('onMessage: message:', message)
     if (!message.url) {
         return console.warn('No message.url')
     }
     let url = new URL(message.url)
     if (url.search.includes('?profile=')) {
-        let profile = url.searchParams.get('profile')
-        console.debug(`Process Profile: ${profile}`)
-        ;(async () => {
-            const url = `https://api-v2.playdrift.com/api/profile/trpc/profile.get?input=%7B%22id%22%3A%22${profile}%22%2C%22game%22%3A%22dominoes%22%7D`
-            const response = await fetch(url)
-            const data = await response.json()
-            console.info('profile data:', data)
+        let profileID = url.searchParams.get('profile')
+        console.debug(`profileID: ${profileID}`)
+        const profileUrl = `https://api-v2.playdrift.com/api/profile/trpc/profile.get?input=%7B%22id%22%3A%22${profileID}%22%2C%22game%22%3A%22dominoes%22%7D`
+        const response = await fetch(profileUrl)
+        const data = await response.json()
+        const profile = data.result.data
+        console.info('profile:', profile)
 
-            let div = document.createElement('div')
-            // div.classList.add(
-            //     'MuiChip-root',
-            //     'MuiChip-outlined',
-            //     'MuiChip-sizeMedium',
-            //     'MuiChip-colorDefault',
-            //     'MuiChip-outlinedDefault'
-            // )
-            let span = document.createElement('span')
-            span.style.color =
-                parseInt(data.result.data.games_won) <
-                    parseInt(data.result.data.games_lost) ||
-                parseInt(data.result.data.rating) < 150
-                    ? 'red'
-                    : 'green'
-            // span.style.color = 'red'
-            span.id = 'stats-text'
-            // span.classList.add('MuiChip-label', 'MuiChip-labelMedium')
-            span.textContent = `Rating: ${data.result.data.rating} - W/L: ${data.result.data.games_won} / ${data.result.data.games_lost}`
-            div.appendChild(span)
-            let root = document
-                .querySelector('.MuiDialogContent-root')
-                .querySelectorAll('.MuiBox-root')[3]
-            root.appendChild(div)
-            // let textData = `${data.result.data.username} - ${span.textContent}`
-            // navigator.clipboard.writeText(textData).then()
+        let div = document.createElement('div')
+        let span = document.createElement('span')
+        span.style.color =
+            parseInt(profile.games_won) < parseInt(profile.games_lost) ||
+            parseInt(profile.rating) < 150
+                ? 'red'
+                : 'green'
+        span.id = 'stats-text'
+        span.textContent = `Rating: ${profile.rating} - W/L: ${profile.games_won} / ${profile.games_lost}`
+        div.appendChild(span)
+        let root = document
+            .querySelector('.MuiDialogContent-root')
+            .querySelectorAll('.MuiBox-root')[3]
+        root.appendChild(div)
 
-            let sendButton = document.createElement('button')
-            sendButton.addEventListener('click', sendClick)
-            sendButton.textContent = 'Send and Close'
-            sendButton.style.marginRight = '15px'
-            sendButton.style.marginBottom = '10px'
-            root.appendChild(sendButton)
+        let sendButton = document.createElement('button')
+        sendButton.addEventListener('click', sendClick)
+        sendButton.textContent = 'Send and Close'
+        sendButton.style.marginRight = '15px'
+        sendButton.style.marginBottom = '10px'
+        root.appendChild(sendButton)
 
-            let copyButton = document.createElement('button')
-            copyButton.addEventListener('click', copyClick)
-            copyButton.textContent = 'Copy'
-            root.appendChild(copyButton)
+        let copyButton = document.createElement('button')
+        copyButton.addEventListener('click', copyClick)
+        copyButton.textContent = 'Copy'
+        root.appendChild(copyButton)
 
-            let username = document.createElement('span')
-            username.id = 'profile-username'
-            username.textContent = data.result.data.username
-            username.hidden = true
-            root.appendChild(username)
-        })()
+        // TODO: Add whole profile to form
+        let username = document.createElement('span')
+        username.id = 'profile-username'
+        username.textContent = profile.username
+        username.hidden = true
+        root.appendChild(username)
     }
     // if (url.pathname.includes('/room/')) {
     //     let room = url.pathname.split('/')[2]
     //     console.debug(`Process Room: ${room}`)
     // }
-})
+}
 
+/**
+ * copyClick Callback
+ * @function saveOptions
+ * @param {MouseEvent} event
+ */
 function copyClick(event) {
+    console.debug('copyClick', event)
     let username = document.getElementById('profile-username').textContent
     let text = document.getElementById('stats-text').textContent
     let data = `${username} - ${text}`
@@ -85,7 +88,13 @@ function copyClick(event) {
     // history.back()
 }
 
+/**
+ * sendClick Callback
+ * @function saveOptions
+ * @param {MouseEvent} event
+ */
 function sendClick(event) {
+    console.debug('sendClick', event)
     let username = document.getElementById('profile-username').textContent
     let text = document.getElementById('stats-text').textContent
     let data = `${username} - ${text}`
