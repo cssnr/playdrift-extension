6fe25dd7cd26951dce0ae45f1be62cb2ea5c530d
Shane <6071159+smashedr@users.noreply.github.com>
2024-03-28 01:46:15 -0700

    Add Copy and Send Buttons

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -31,20 +31,19 @@
 // }
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log('message:', message)
+    console.debug('message:', message)
     if (!message.url) {
-        return
+        return console.warn('No message.url')
     }
     let url = new URL(message.url)
     if (url.search.includes('?profile=')) {
         let profile = url.searchParams.get('profile')
-        console.log(`Process Profile: ${profile}`)
+        console.debug(`Process Profile: ${profile}`)
         ;(async () => {
             const url = `https://api-v2.playdrift.com/api/profile/trpc/profile.get?input=%7B%22id%22%3A%22${profile}%22%2C%22game%22%3A%22dominoes%22%7D`
             const response = await fetch(url)
             const data = await response.json()
-            console.log('profile data:', data)
-            // console.log(`rating: ${data.result.data.rating}`)
+            console.info('profile data:', data)
 
             let div = document.createElement('div')
             div.classList.add(
@@ -59,40 +58,37 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
             span.classList.add('MuiChip-label', 'MuiChip-labelMedium')
             span.textContent = `Rating: ${data.result.data.rating} - W/L: ${data.result.data.games_won} / ${data.result.data.games_lost}`
             div.appendChild(span)
-            // console.log('div:', div)
-            let copy = `${data.result.data.username} - ${span.textContent}`
-            navigator.clipboard.writeText(copy).then()
-
             let root = document
                 .querySelector('.MuiDialogContent-root')
                 .querySelectorAll('.MuiBox-root')[3]
-            // console.log('root:', root)
             root.appendChild(div)
+            // let textData = `${data.result.data.username} - ${span.textContent}`
+            // navigator.clipboard.writeText(textData).then()
 
-            // let copy = document.createElement('button')
-            // copy.addEventListener('click', copyButton)
-            // copy.textContent = 'Copy'
-            // root.appendChild(copy)
+            let sendButton = document.createElement('button')
+            sendButton.addEventListener('click', sendClick)
+            sendButton.textContent = 'Send to Chat'
+            root.appendChild(sendButton)
 
-            // let send = document.createElement('button')
-            // send.addEventListener('click', sendButton)
-            // send.textContent = 'Send'
-            // root.appendChild(send)
+            let copyButton = document.createElement('button')
+            copyButton.addEventListener('click', copyClick)
+            copyButton.textContent = 'Copy'
+            root.appendChild(copyButton)
 
-            // let username = document.createElement('span')
-            // username.id = 'profile-username'
-            // username.textContent = data.result.data.username
-            // username.hidden = true
-            // root.appendChild(username)
+            let username = document.createElement('span')
+            username.id = 'profile-username'
+            username.textContent = data.result.data.username
+            username.hidden = true
+            root.appendChild(username)
         })()
     }
-    if (url.pathname.includes('/room/')) {
-        let room = url.pathname.split('/')[2]
-        console.log(`Process Room: ${room}`)
-    }
+    // if (url.pathname.includes('/room/')) {
+    //     let room = url.pathname.split('/')[2]
+    //     console.debug(`Process Room: ${room}`)
+    // }
 })
 
-function copyButton(event) {
+function copyClick(event) {
     let username = document.getElementById('profile-username').textContent
     let text = document.getElementById('stats-text').textContent
     let data = `${username} - ${text}`
@@ -100,12 +96,17 @@ function copyButton(event) {
     navigator.clipboard.writeText(data).then()
 }
 
-function sendButton(event) {
+function sendClick(event) {
     let username = document.getElementById('profile-username').textContent
     let text = document.getElementById('stats-text').textContent
     let data = `${username} - ${text}`
     console.log(`sending text: ${data}`)
-    let textarea = document.getElementById(':re:')
+    // let textarea = document.querySelector(
+    //     '.MuiInputBase-input .MuiOutlinedInput-input .MuiInputBase-inputMultiline'
+    // )
+    let textarea = document.querySelectorAll(
+        'textarea[aria-invalid="false"]'
+    )[1]
     textarea.value = data
     let button = document.querySelector('button[aria-label="send message"]')
     button.click()
