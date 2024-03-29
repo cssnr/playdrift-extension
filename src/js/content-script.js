756f4c6504b5d1c68b00a175b55ab61bced7d77b
Shane <6071159+smashedr@users.noreply.github.com>
2024-03-28 17:41:59 -0700

    Add content-script.js functions

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -26,52 +26,72 @@ async function onMessage(message, sender, sendResponse) {
     }
     let url = new URL(message.url)
     if (url.search.includes('?profile=')) {
-        let profileID = url.searchParams.get('profile')
+        const profileID = url.searchParams.get('profile')
         console.debug(`profileID: ${profileID}`)
-        const profileUrl = `https://api-v2.playdrift.com/api/profile/trpc/profile.get?input=%7B%22id%22%3A%22${profileID}%22%2C%22game%22%3A%22dominoes%22%7D`
-        const response = await fetch(profileUrl)
-        const data = await response.json()
-        const profile = data.result.data
-        console.info('profile:', profile)
-
-        let div = document.createElement('div')
-        let span = document.createElement('span')
-        span.style.color =
-            parseInt(profile.games_won) < parseInt(profile.games_lost) ||
-            parseInt(profile.rating) < 150
-                ? 'red'
-                : 'green'
-        span.id = 'stats-text'
-        span.textContent = `Rating: ${profile.rating} - W/L: ${profile.games_won} / ${profile.games_lost}`
-        div.appendChild(span)
-        let root = document
-            .querySelector('.MuiDialogContent-root')
-            .querySelectorAll('.MuiBox-root')[3]
-        root.appendChild(div)
-
-        let sendButton = document.createElement('button')
-        sendButton.addEventListener('click', sendClick)
-        sendButton.textContent = 'Send and Close'
-        sendButton.style.marginRight = '15px'
-        sendButton.style.marginBottom = '10px'
-        root.appendChild(sendButton)
-
-        let copyButton = document.createElement('button')
-        copyButton.addEventListener('click', copyClick)
-        copyButton.textContent = 'Copy'
-        root.appendChild(copyButton)
-
-        // TODO: Add whole profile to form
-        let username = document.createElement('span')
-        username.id = 'profile-username'
-        username.textContent = profile.username
-        username.hidden = true
-        root.appendChild(username)
+        const profile = await getProfile(profileID)
+        console.debug('profile:', profile)
+        updateProfile(profile)
     }
     // if (url.pathname.includes('/room/')) {
     //     let room = url.pathname.split('/')[2]
     //     console.debug(`Process Room: ${room}`)
     // }
+}
+
+/**
+ * copyClick Callback
+ * @function saveOptions
+ * @param {string} profileID
+ * @returns {Object}
+ */
+async function getProfile(profileID) {
+    const profileUrl = `https://api-v2.playdrift.com/api/profile/trpc/profile.get?input=%7B%22id%22%3A%22${profileID}%22%2C%22game%22%3A%22dominoes%22%7D`
+    const response = await fetch(profileUrl)
+    const data = await response.json()
+    const profile = data.result.data
+    console.info('profile:', profile)
+    return profile
+}
+
+/**
+ * copyClick Callback
+ * @function saveOptions
+ * @param {Object} profile
+ */
+function updateProfile(profile) {
+    let div = document.createElement('div')
+    let span = document.createElement('span')
+    span.style.color =
+        parseInt(profile.games_won) < parseInt(profile.games_lost) ||
+        parseInt(profile.rating) < 150
+            ? 'red'
+            : 'green'
+    span.id = 'stats-text'
+    span.textContent = `Rating: ${profile.rating} - W/L: ${profile.games_won} / ${profile.games_lost}`
+    div.appendChild(span)
+    let root = document
+        .querySelector('.MuiDialogContent-root')
+        .querySelectorAll('.MuiBox-root')[3]
+    root.appendChild(div)
+
+    let sendButton = document.createElement('button')
+    sendButton.addEventListener('click', sendClick)
+    sendButton.textContent = 'Send and Close'
+    sendButton.style.marginRight = '15px'
+    sendButton.style.marginBottom = '10px'
+    root.appendChild(sendButton)
+
+    let copyButton = document.createElement('button')
+    copyButton.addEventListener('click', copyClick)
+    copyButton.textContent = 'Copy'
+    root.appendChild(copyButton)
+
+    // TODO: Add whole profile to form
+    let username = document.createElement('span')
+    username.id = 'profile-username'
+    username.textContent = profile.username
+    username.hidden = true
+    root.appendChild(username)
 }
 
 /**
